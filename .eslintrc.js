4bd449deda15fa93218633aa79f60123ef2f5504
Luke Morales <lukemorales@live.com>
2019-07-14 02:15:05 -0300

    chore: ESLint and Prettier Adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  rules: {
-    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  },
 };
